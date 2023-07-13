c0c9b882cc9baa2ec444a0e4b4d853f658197ce0
Miliano Fernandes de Oliveira Junior <69368192+MilianoJunior@users.noreply.github.com>
2023-07-13 17:48:41 -0300

    update bind address

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
+import os
 from fastapi import FastAPI
 
 app = FastAPI()
 
+bind_address = os.getenv('BIND', '0.0.0.0')
 
+
+# an9atbr9.up.railway.app
 @app.get("/")
 async def root():
     return {"message": "EngeSEP API"}
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.get("/autentic/{user}/{password}")
+async def say_hello(name: str, password: str):
+    if password == "123":
+        return {"message": f"Hello {name}"}
+    return {"message": "Password incorrecto"}
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run("main:app", host=bind_address, port=8000, reload=True)
+
