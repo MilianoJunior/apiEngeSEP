bbdafb19c49c3638589ba55c41a31a38d2835373
MilianoJunior <jrmfilho37@gmail.com>
2023-08-29 10:23:11 -0300

    update exposed

--- libs/connection.py
+++ libs/connection.py
@@ -3,10 +3,6 @@
 # import os
 
 
-# DATABASE_URL = os.environ.get('MYSQL_URL',"mysql+pymysql://root:AIgi26lIFr70Rz6uGUbQ@containers-us-west-66.railway.app:7438/railway")  # ou o nome apropriado da variável de ambiente
-# # DATABASE_URL = "mysql+pymysql://root:AIgi26lIFr70Rz6uGUbQ@containers-us-west-66.railway.app:7438/railway"
-# print('URL: ',DATABASE_URL)
-# engine = create_engine(DATABASE_URL)
 
 class DatabaseManager():
     def __init__(self, table):
